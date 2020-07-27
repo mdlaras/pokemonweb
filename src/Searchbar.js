eacfdacd99979cf55ccdd2e3744bc63ccce7d9a9
mdlaras <52792426+mdlaras@users.noreply.github.com>
2020-07-27 13:43:11 +0700

    Making Searchbar component

---
+++ src/Searchbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Searchbar extends Component{
+    render(){
+        return(
+            <input className='search'>Enter Pokemon Name</input>
+        )
+    }
+}
+
+export default Searchbar
