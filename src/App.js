cc86a46bacb427a66f844ea2604ac83ec9118e15
mdlaras <52792426+mdlaras@users.noreply.github.com>
2020-07-27 13:47:06 +0700

    creating Card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Searchbar from './Searchbar'
+import Card from './Card'
 
 class App extends React.Component {
   render(){
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default Card
