f01613729073d9b0d20f69a2fde33ac17ce99c1e
mdlaras <52792426+mdlaras@users.noreply.github.com>
2020-07-28 19:52:22 +0700

    base application finish

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = ({name, pokeurl}) => {
     var newurl = String(pokeurl)
-    var pokeorder
-    fetch(newurl).then(response=>{return response.json()}).then(poki=>{pokeorder=poki.order;return pokeorder})
+    const [pokeorder, pokeorderset] = useState(0)
+    fetch(newurl).then(response=>{return response.json()}).then(poki=>{pokeorderset(poki.order)})
     return(
         <div>
-            <img alt= 'sprite' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeorder}.png`}/>
+            <img alt= 'sprite' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeorder}.png`} width='200' height='200'/>
             <h1>{name}</h1>
-            <h2>{pokeorder}</h2>
         </div>
     )
 }
