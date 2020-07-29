dada784aad52868e16c0e3354f6afff7b577ef3f
mdlaras <mariadlarasati@gmail.com>
2020-07-30 01:02:02 +0700

    Updates

---
+++ precache-manifest.af2d73e8c1c8933c72e10e108d177d7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b68bb80b32dad973b154c94eef07aa",
+    "url": "/pokemonweb/index.html"
+  },
+  {
+    "revision": "40c63589b370a59c8965",
+    "url": "/pokemonweb/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7b9c41a0a135de56f4b0",
+    "url": "/pokemonweb/static/css/main.c59fc438.chunk.css"
+  },
+  {
+    "revision": "40c63589b370a59c8965",
+    "url": "/pokemonweb/static/js/2.35a11f07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemonweb/static/js/2.35a11f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9c41a0a135de56f4b0",
+    "url": "/pokemonweb/static/js/main.729d2f98.chunk.js"
+  },
+  {
+    "revision": "f9a01da81c60b53c971c",
+    "url": "/pokemonweb/static/js/runtime-main.2ad6cd09.js"
+  }
+]);
