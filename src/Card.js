f43ab7bec35fb2e78fa53ece5f5d4b832268087e
mdlaras <mariadlarasati@gmail.com>
2020-07-29 23:51:57 +0700

    changing some things

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
 
 const Card = ({name, pokeurl}) => {
-    var newurl = String(pokeurl)
     const [pokeorder, pokeorderset] = useState(0)
-    fetch(newurl).then(response=>{return response.json()}).then(poki=>{pokeorderset(poki.order)})
+    fetch(pokeurl).then(response=>{return response.json()}).then(poki=>{pokeorderset(poki.order)})
     return(
         <div class='bg-silver dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt= 'sprite' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeorder}.png`} width='200' height='200'/>
--- src/CardList.js
+++ src/CardList.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Card from './Card'
 
+const pokelink = 'https://pokeapi.co/api/v2/pokemon/'
 const CardList = ({pokemon}) => {
     const CardComponent = pokemon.map((i) => {
-        return <Card name = {i.name} pokeurl = {i.url}/>
+        return <Card name = {i.name} pokeurl = {pokelink + i.name }/>
     })
     return (
         <div>
