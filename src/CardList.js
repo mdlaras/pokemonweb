fd74406e75b6773591734fb2d63a9293177a586c
mdlaras <mariadlarasati@gmail.com>
2020-07-30 00:37:03 +0700

    fixing pokemon id

--- src/CardList.js
+++ src/CardList.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Card from './Card'
 
-const pokelink = 'https://pokeapi.co/api/v2/pokemon/'
 const CardList = ({pokemon}) => {
     const CardComponent = pokemon.map((i) => {
-        return <Card name = {i.name} pokeurl = {pokelink + i.name }/>
+        return <Card name = {i.name} pokeurl = {i.url}/>
     })
     return (
         <div>
